fe263acd88a50044b008d68ef833444dafb377b2
Switch <72192213+Switch1220@users.noreply.github.com>
2025-08-02 10:19:18 +0000

    Update index.html paths, add CNAME for custom domain, and adjust Vite base configuration

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/cyberstart/",
+  // base: "/cyberstart/",
   build: {
     outDir: "../docs",
   },
